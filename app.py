f420576de32555f3700092d61e9b9dde8bc63ba8
Georges <92797658+georgrmat@users.noreply.github.com>
2023-12-13 10:55:32 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+def home():
+    st.title("Home Page")
+    st.sidebar.write("Sidebar content for Home Page")
+    st.write("Welcome to the Home Page!")
+
+def page_one():
+    st.title("Page 1")
+    st.sidebar.write("Sidebar content for Page 1")
+    st.write("This is Page 1.")
+
+def page_two():
+    st.title("Page 2")
+    st.sidebar.write("Sidebar content for Page 2")
+    st.write("You are now on Page 2.")
+
+def main():
+    pages = ["Home", "Page 1", "Page 2"]
+    choice = st.sidebar.selectbox("Select Page", pages)
+
+    if choice == "Home":
+        home()
+    elif choice == "Page 1":
+        page_one()
+    elif choice == "Page 2":
+        page_two()
+
+if __name__ == "__main__":
+    main()
